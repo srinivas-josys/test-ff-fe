2c92675b3d6819b226cd31f095aa0c4683e97c5b
Srinivas R <srinivas.r@josys.com>
2025-05-30 10:08:07 +0530

    Version Bump

---
+++ debug-pkg.ts
@@ -0,0 +1,2 @@
+import * as pkg from "@josys-src/josys-ff-ui-commons";
+console.log("PACKAGE RESOLVED ✅", pkg);
---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ["@josys-src/josys-ff-ui-commons"],
+};
+
+module.exports = nextConfig;
